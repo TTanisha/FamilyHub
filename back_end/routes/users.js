d24e49e041a84c752a2562867c92ee0a898a2b16
Freyja <fkristjanson@outlook.com>
2023-02-02 15:24:42 -0600

    Initial database setup

---
+++ back_end/routes/users.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const userRoutes = express.Router();
+
+const database = require("../database/connection");
+
+module.exports = userRoutes;
--- back_end/server.js
+++ back_end/server.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const cors = require("cors"); // Node.js package that allows cross origin resource sharing
+
+require("dotenv").config({path: "config.env"}); // load environment variables
+const port = process.env.PORT || 5000; // access the port variable from config.env
+
+app.use(cors()); // cors middleware
+app.use(express.json()); // express middleware, handles body parsing 
+
+// routes
+app.use(require("./routes/users"));
+
+// driver connection
+const database = require("./database/connection");
 
 app.get('/api', (req, res) => {
   res.json({"message": ["Message", "from", "backend", "server"]})
-})
+});
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`)
-})
+  // when server starts, connect to the database
+  database.connectToServer(function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Connected.");
+    }
+  });
+});

e8d628613d6c3b87f9afdbc446249bd5eae06167
Freyja <fkristjanson@outlook.com>
2023-02-03 15:25:02 -0600

    Use mongoose for database connection

--- back_end/server.js
+++ back_end/server.js
@@ -1,11 +1,23 @@
-const express = require("express");
 const app = require("./app");
 
+const mongoose = require("mongoose");
 require("dotenv").config({path: "config.env"}); // load environment variables
+
 const PORT = process.env.PORT || 5000; // access the port variable from config.env
 
-// driver connection
-const database = require("./database/connection");
+// Database connection
+const DB = process.env.FAMILYHUB_DB_URI;
+mongoose.set('strictQuery', false);// Preparation for deprecation 
+const connectionOptions = {
+  // Required due to changes in the MongoDB Node.js driver
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+}
+
+mongoose.connect(DB, connectionOptions).then(
+  () => {console.log("Successfully connected to MongoDB.")},
+  err => {console.error("Unable to connect to MongoDB.", err.message)}
+);
 
 // Demo response to make sure it is working
 app.get('/', (req, res) => {
@@ -14,15 +26,7 @@ app.get('/', (req, res) => {
 
 // Run the server
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-  // when server starts, connect to the database
-  database.connectToServer(function (err) {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("Connected.");
-    }
-  });
+  console.log("Server running on port:", PORT);
 });
 
 module.exports = app;

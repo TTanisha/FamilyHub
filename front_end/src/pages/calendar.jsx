ddc1acabb28af482c159d75f9a466b05a361fa41
Jasmine Tabuzo <tabuzoj@myumanitoba.ca>
2023-02-04 02:15:39 -0600

    83 - Added basic nav bar functionality
    - added a nav bar and router
    - updated the page title to FamilyHub

---
+++ front_end/src/pages/calendar.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState }  from 'react';
+
+const Calendar = () => {
+  return (
+    <div className="calendar-component">
+     Calendar in progress :)
+    </div>
+  );
+};
+  
+export default Calendar;

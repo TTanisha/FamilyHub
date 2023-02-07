831603c6a5bb314c7d24029a8bee2de669e7fbff
Juan Armijos <64816257+armijosj@users.noreply.github.com>
2023-02-07 17:08:52 -0600

    Update userController to include all fields in registration

--- back_end/controllers/userController.js
+++ back_end/controllers/userController.js
@@ -5,20 +5,21 @@ const Users = require("../models/userModel");
 
 exports.registerUser = async(req, res, next) => { 
     
-    console.log("Sould have something-> ");
-
-    console.log(req.body);
-    const { username, password } = req.body
-
-
+    const { username, password, firstName, lastName, birthday, eMail } = req.body
     if (password.length < 6) {
         return res.status(400).json({ message: "Password less than 6 characters" })
     }
+
+    // TO DO: further input validation
 
     try {
           await Users.create({
             username,
             password,
+            firstName, 
+            lastName,
+            birthday, 
+            eMail
           }).then(user =>
             res.status(200).json({
               message: "User successfully created",
@@ -26,6 +27,7 @@ exports.registerUser = async(req, res, next) => {
             })
           )
     } catch (err) {
+      console.log(err)
         res.status(401).json({
             message: "User not successful created",
             error: err.mesage,

6d9762f6ce029f8d7887d079df9afe0f5e2b64c4
Juan Armijos <64816257+armijosj@users.noreply.github.com>
2023-02-09 15:17:03 -0600

    Develped getUser API function and started on unit testing of User Account

--- back_end/routes/userRoutes.js
+++ back_end/routes/userRoutes.js
@@ -6,5 +6,8 @@ const userController = require("../controllers/userController");
 
 // send each request to corresponding controller function
 router.route("/registerUser").post(userController.registerUser);
+router.route("/getUser").get(userController.getUser);
+
+
 
 module.exports = router;
---
+++ back_end/test/user.test.js
@@ -0,0 +1,59 @@
+const user = require('../models/userModel');
+const app = require('../app');
+let supertest = require('supertest');
+let request = supertest(app);
+const mongoose = require('mongoose');
+require("dotenv").config({path: "config.env"}); // load environment variables
+
+
+// beforeAll( async ()=> {
+//     jest.useFakeTimers('legacy')
+    
+//     // Database connection
+//     const DB = process.env.FAMILYHUB_DB_URI;
+//     mongoose.set('strictQuery', false);// Preparation for deprecation 
+//     const connectionOptions = {
+//         // Required due to changes in the MongoDB Node.js driver
+//         useNewUrlParser: true, 
+//         useUnifiedTopology: true
+//     }
+
+//     mongoose.connect(DB, connectionOptions).then(
+//         () => {console.log("Successfully connected to MongoDB.")},
+//         err => {console.error("Unable to connect to MongoDB.", err.message)}
+//     );
+// })
+
+
+// afterAll( async () => {
+//     //make we have deleted the test user from the database
+//     await user.findOneAndDelete({username: 'TestUser'})
+//     await mongoose.connection.close();
+// })
+
+
+describe("test1", () => {
+    test('1 should be 1 ', () => {
+      expect(1).toBe(1);
+    });
+});
+
+
+
+/* IN DEVELOPMENT
+describe('Enter new User',() =>{
+    test('User has provided all details correct',async () => {
+        const response = await request.post('/api/users/registerUser').send({
+            firstName: 'TestFirstName',
+            lastName: 'TestLastName',
+            eMail: 'TestEmail@com.ca',
+            username: 'TestUser',
+            password: 'Test123@',
+            birthday: "31/01/2001",
+        }) 
+        expect(response.statusCode).toBe(201);
+        //await user.findOneAndDelete({username: 'TestUser'});    
+    });
+} )
+
+*/

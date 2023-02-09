3c2650051197cd4b7f82e122011b50e268952261
Freyja <fkristjanson@outlook.com>
2023-02-08 19:35:16 -0600

    Update/create user schema

--- back_end/models/userModel.js
+++ back_end/models/userModel.js
@@ -1,37 +1,52 @@
 const mongoose = require("mongoose");
 
-// set up the mongoDB schema
-
-/* For example
-const userSchema = mongoose.Schema({
-    firstName: {
-        type: String,
-        required: [true, "A user must have FirstName"],
-    },
-    lastName: {
-        type: String,
-        required: [true, "A user must have lastName"],
-    },
-    eMail: {
-        type: String,
-        required: [true, "A user must have Email"],
-        unique: true,
-    },
-    username: {
-        type: String,
-        required: [true, "A user must have username"],
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: [true, "A user must have password"],
-    },
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email required."],
+    unique: true
+  },
+  firstName: {
+    type: String,
+    required: [true, "First name is required."],
+    minLength: [1, "First name must have at least one character."]
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last name is required."],
+    minLength: [1, "Last name must have at least one character."]
+  },
+  birthday: {
+    type: Date, 
+    required: [true, "A user must have a birthday."]
+  },
+  nickname: {
+    type: String,
+    required: [false]
+  },
+  pronouns: {
+    type: String, 
+    required: [false]
+  },
+  displayEmail: {
+    type: String,
+    required: [false]
+  },
+  address: {
+    type: String,
+    required: [false]
+  },
+  cellNumber: {
+    type: Number,
+    required: [false]
+  },
+  homeNumber: {
+    type: Number,
+    required: [false]
+  },
+  timestamps: true // adds createdAt and updatedAt Date fields
 });
 
-
-*/
-
-
-// const Users = mongoose.model("Users", userSchema);
+const User = mongoose.model("User", userSchema);
 // const TestUsers = mongoose.model("TestUsers", userSchema);
-// (module.exports = Users), TestUsers;
+module.exports = User;

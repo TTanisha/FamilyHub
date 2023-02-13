7d4daa1ef1508ef51efca6a6a1c4a514d1dab6dc
Tanisha Turner <rachelle.t@live.ca>
2023-02-13 17:15:16 -0600

    changed date picker from toastUI to react-datepicker

--- front_end/src/pages/signPages/signUp.jsx
+++ front_end/src/pages/signPages/signUp.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import DatePicker from 'tui-date-picker'; 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import './sign.css';
-  
+
+
 const SignUp = () => {
     const[email, setEmail] = React.useState('');
     const[password, setPassword] = React.useState('');
+    const[birthDate, SetBirthDate] = React.useState(new Date());
   return (
     <div class="sign-up">
         <h1>Sign-Up</h1>
@@ -14,14 +17,7 @@ const SignUp = () => {
     <label>
         Password: <input value={password} onChange={p => setPassword(p.target.value)}/>
     </label>
-    <label>
-      <div class="code-hmtml">
-        <div class="tui-datepicker-input tui-datetime-input tui-has-focus">
-          <input type="text" id="datepicker-input" aria-label="Date-Time"></input>
-            <span class="tui-ico-date"></span>
-        </div>
-      </div>
-    </label>
+    <DatePicker dateFormat="dd/MM/yyyy" selected={birthDate} onChange={(date)=> SetBirthDate(date)}/>
     {/* {email !== '' &&
         <p>Your name is {email}.</p>
       } */}

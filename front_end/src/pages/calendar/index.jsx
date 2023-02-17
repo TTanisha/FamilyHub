63fa3cb443943527e8187fecb99c34a0c819cf63
Jasmine Tabuzo <tabuzoj@myumanitoba.ca>
2023-02-17 01:58:29 -0600

    Add fake events to calendar

--- front_end/src/pages/calendar/index.jsx
+++ front_end/src/pages/calendar/index.jsx
@@ -11,6 +11,27 @@ let tuiCalendar = new TUICalendar('#calendar', {
   isReadOnly: true
 });
 
+let events = [
+  {
+    id: '1',
+    calendarId: '1',
+    title: 'Valentines Day',
+    category: 'allday',
+    start: '2023-02-14T22:30:00+09:00',
+    end: '2023-02-14T22:30:00+09:00',
+    isAllDay: true,
+  },
+  {
+    id: '1',
+    calendarId: '1',
+    title: 'Jane\'s Birthday',
+    category: 'allday',
+    start: '2023-02-02T22:30:00+09:00',
+    end: '2023-02-02T22:30:00+09:00',
+    isAllDay: true,
+  },
+];
+
 let currDate = new Date();
 
 function createCalendar() {
@@ -55,10 +76,17 @@ function setCalendarTitle() {
   document.getElementById("renderRange").innerHTML = string;
 }
 
+function setCalendarEvents() {
+  tuiCalendar.createEvents(events);
+
+  tuiCalendar.render();
+}
+
 const Calendar = () => {
   useEffect(() => {
     createCalendar();
     setCalendarTitle();
+    setCalendarEvents();
   });
 
   return (

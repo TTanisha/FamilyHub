4670388409b9f5df73d1c375548e0d407b0d549f
Jasmine Tabuzo <tabuzoj@myumanitoba.ca>
2023-02-17 01:15:46 -0600

    Fix family group page bug when no users found
    - display "You are not part of any family groups"
    - check arrays before trying to access data
    - removed duplicate getUserById method

--- front_end/src/components/familyGroup/familyGroup.jsx
+++ front_end/src/components/familyGroup/familyGroup.jsx
@@ -4,25 +4,30 @@ import axios from 'axios';
   
 const FamilyGroup = (props) => {
   const [groupMembers, setGroupMembers] = useState([]);
+  const [loading, setLoading] = useState(true);
   
   useEffect(() => {
     const CancelToken = axios.CancelToken;
     const source = CancelToken.source();
 
-    Promise.all(props.groupMembers.map((userId) => (
-      //retrieve group objects using ID 
-      axios.post("http://localhost:8080/api/users/getUserById", {userId: userId}, {cancelToken: source.token})
+    setLoading(true);
+    Promise.all(props.groupMembers.map(async (userId) => (
+      //retrieve users using ID 
+      await axios.post("http://localhost:8080/api/users/getUserById", {id: userId}, {cancelToken: source.token})
       .then(function(response)
       {
           if(response.data.status === "success")
           {          
-            const newUser = response.data.data;
+            const newUser = response.data.data.user;
             setGroupMembers(prev => [...prev, newUser]);
             return response.data;
           }
       })
       .catch(function (error) {
-      })
+        if(error.message!='canceled') {
+          console.log(error)
+        }})
+      .finally(() => setLoading(false))
     )))
 
     return () => {
@@ -35,7 +40,7 @@ const FamilyGroup = (props) => {
     <div className="familyContainer">
       <div className="familyName"> {props.groupName} </div>
       {
-        groupMembers.map((userData) => ( <div className="memberName"> {userData.user[0].firstName} {userData.user[0].lastName}</div>)) 
+        !loading && (groupMembers?.map((userData) => ( loading ? null : <div className="memberName"> {userData?.firstName} {userData?.lastName}</div>))) 
       }
     </div>
   );

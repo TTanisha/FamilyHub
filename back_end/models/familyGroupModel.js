f7036ea0d4dad7bb45de48f9db107fbf91f91636
Freyja <fkristjanson@outlook.com>
2023-02-23 16:23:40 -0800

    Add collection name, whitespace and tab size

--- back_end/models/familyGroupModel.js
+++ back_end/models/familyGroupModel.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 
 // mongoDB schema
 const familyGroupSchema = mongoose.Schema({
-    groupName: {
-        type: String,
-        required: [true, "A family group must have groupName"],
-        minLength: [1, "Family group name must have at least one character."]
-    },
-    groupMembers: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: [false]
-    }]
+  groupName: {
+    type: String,
+    required: [true, "A family group must have groupName"],
+    minLength: [1, "Family group name must have at least one character."]
+  },
+  groupMembers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [false]
+  }]
 });
 
-const FamilyGroups = mongoose.model("FamilyGroups", familyGroupSchema);
+const FamilyGroups = mongoose.model("FamilyGroups", familyGroupSchema, collection = "Family Groups");
 module.exports = FamilyGroups;

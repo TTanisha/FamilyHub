f9d8cc05063e93709afe35dc8b8ee832b62cba23
Jasmine Tabuzo <tabuzoj@myumanitoba.ca>
2023-03-15 02:12:21 -0500

    Update sign-in and sign-up UI

--- front_end/src/pages/signPages/signUp.jsx
+++ front_end/src/pages/signPages/signUp.jsx
@@ -1,46 +1,70 @@
-import React, { useEffect } from 'react';
-import DatePicker from "react-datepicker";
+import React from 'react';
 import "react-datepicker/dist/react-datepicker.css";
 import './sign.css';
 import axios from 'axios';
+import {Button, Grid, Input, Spacer} from "@nextui-org/react";
 
 const SignUp = () => {
   const[firstName, setFirstName] = React.useState('');
   const[lastName, setlastName] = React.useState('');
   const[email, setEmail] = React.useState('');
   const[password, setPassword] = React.useState('');
-  const[birthDate, SetBirthDate] = React.useState(new Date());
-  // const[signedUp, setSignedUp] = React.useState(false);
-  // const[hasError, setHasError] = React.useState(false);
-  // const[errorMessage, setErrorMessage] = React.useState("");
+  const[birthDate, setBirthDate] = React.useState(null);
 
   return (
     <div className="sign-up">
         <h1>Sign-Up</h1>
-        <label>
-        First Name: <input value={firstName} onChange={fn => setFirstName(fn.target.value)}/>
-    </label>
-    <label>
-        Last Name: <input value={lastName} onChange={ln => setlastName(ln.target.value)}/>
-    </label>
-      <label>
-        Email Address: <input value={email} onChange={e => setEmail(e.target.value)}/>
-    </label>
-    <label>
-        Password: <input type="password" value={password} onChange={p => setPassword(p.target.value)}/>
-    </label>
-    <label>Birthday (dd/mm/yyyy): </label>
-    <DatePicker dateFormat="dd/MM/yyyy" selected={birthDate} onChange={(date)=> SetBirthDate(date)}/>
-      <button className="sign-in" onClick={() => signUserUp({firstName: firstName, lastName: lastName, email: email, password:password, birthday: birthDate})}>Sign-Up</button>
-      {/* <div> {hasError && <div>{errorMessage} </div> } </div>
-   
-    
-    <div> {signedUp && <div>You've successfully created a new account! Return to the login page. 
-          <button className="sign-in" onClick={() => {window.location.href = '/'}}>Return to login page</button></div> } </div> */}
+        <form>
+          <Grid.Container direction='column' gap={3} alignContent="left">
+            <Grid> 
+              <Input 
+                label="First Name:"
+                helperText="required"
+                onChange={fn => setFirstName(fn.target.value)}>
+              </Input>
+            </Grid>
+            <Grid>
+              <Input 
+                label="Last Name:"
+                helperText="required"
+                onChange={ln => setlastName(ln.target.value)}>
+              </Input>
+            </Grid>
+            <Grid>
+              <Input 
+                label="Email Address:"
+                helperText="required"
+                onChange={e => setEmail(e.target.value)}>
+              </Input>
+            </Grid>
+            <Grid>
+              <Input 
+                label="Password:"
+                helperText="required, must be at least 6 characters"
+                type="password"
+                onChange={p => setPassword(p.target.value)}>
+              </Input>
+            </Grid>
+            <Grid>
+              <Input 
+                label="Birthday:"
+                helperText="required"
+                type="date"
+                onChange={(date) => setBirthDate((new Date(date.target.value)).toISOString())}>
+              </Input>
+            </Grid>
+          </Grid.Container>
+        </form>
+        <Spacer y={2}/>
+        <Button 
+          className="sign-in" 
+          onClick={() => signUserUp({firstName: firstName, lastName: lastName, email: email, password:password, birthday: birthDate})}
+        >
+          Create account
+        </Button>
     </div>
   );
 };
-
 
 async function signUserUp(props) {
   await axios.post("http://localhost:8080/api/users/registerUser", (props))
@@ -49,19 +73,10 @@ async function signUserUp(props) {
        if(response.data.status === "success")
        {
         window.location.href = '/signup-success';
-        //  setSignedUp(true);
-       } else {
-        window.alert('Invalid details. Please try again.');
-         //console.log(response.data.message);
-        //  setHasError(true);
-        //  setErrorMessage(response.data.message);
-       }
+       } 
    }).catch(function (error) {
        console.log(error);
-       window.alert('Could not create account. Please try again.');
-      //  setHasError(true);
-      //  setErrorMessage(error);
+       window.alert(error.response.data.message);
    })};
   
-
 export default SignUp;

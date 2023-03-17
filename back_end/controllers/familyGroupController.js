36d5894f2681ffa8499892eb8c429616bbe9c396
Freyja <fkristjanson@outlook.com>
2023-03-17 16:20:39 -0500

    Add family group integration tests, restructure group controller

--- back_end/controllers/familyGroupController.js
+++ back_end/controllers/familyGroupController.js
@@ -1,226 +1,216 @@
 const FamilyGroups = require("../models/familyGroupModel");
 const Users = require("../models/userModel");
-const errorToJSON = require('error-to-json');
-const { db } = require("../models/familyGroupModel");
+const { ObjectId } = require("mongodb");
 
 // Include controller logic
-exports.createFamilyGroup = async(req, res, next) => { 
-    try {
-      const { groupName } = req.body
-          await FamilyGroups.create({
-            groupName
-          }).then(group =>
-            res.status(200).json({
-              status: "success",
-              message: "Family Group (ID: " + group._id +") successfully created",
-              group,
-            })
-          )
-    } catch (err) {
-      //parsing error to JSON
-      var reason = "";
-      var dbError = errorToJSON.parse(err);
-      console.log("The following error occurred:" + dbError);
-
-      res.status(401).json({
-          message: reason + "Family group not successfully created",
-        })
+exports.createFamilyGroup = async (req, res) => {
+  try {
+    const { groupName } = req.body;
+    const group = await FamilyGroups.create({
+      groupName,
+    });
+    if (group !== null) {
+      res.status(200).send({
+        status: "success",
+        message: "Family Group (ID: " + group._id + ") successfully created",
+        group,
+      })
+    } else {
+      throw new Error(message = "Unable to create a group with name: " + groupName);
     }
-    
+  } catch (err) {
+    res.status(401).send({
+      message: err.message,
+      description: "Family group not successfully created",
+    });
+  }
 };
 
-exports.getFamilyGroup = async(req, res) => {
+/**
+ *
+ * @param {ObjectId} req
+ * @param {JSON} res
+ */
+exports.getFamilyGroup = async (req, res) => {
   try {
-    
-    const { groupId } = req.body
-    const group = await FamilyGroups.findById({ _id: groupId });
-    reason = "";
+    const { groupId } = req.body;
+    const group = await FamilyGroups.findById(groupId);
 
     if (group == null) {
-      reason = "Family Group not found";
-      throw err;
+      throw new Error((message = "Group not found for id: " + groupId));
     }
 
-    res.status(200).json({
-        status: "success",
-        message: "Found group",
-        data: {
-            group: group,
-        },
+    res.status(200).send({
+      status: "success",
+      message: "Found group",
+      data: {
+        group: group,
+      },
     });
   } catch (err) {
-
-    res.status(404).json({
-        status: "fail",
-        message: "Group not found",
+    res.status(404).send({
+      status: "fail",
+      message: err.message,
+      description: "Group not found",
     });
   }
 };
 
 exports.addMemberToFamilyGroup = async (req, res) => {
-    try {
+  try {
+    const { groupId, memberEmail } = req.body;
+    let group = await FamilyGroups.findById(groupId);
+    let member = await Users.findOne({ email: memberEmail });
 
-        const { groupId, memberEmail } = req.body
-        const group = await FamilyGroups.findOne({ _id: groupId });
-        const member = await Users.findOne({ email: memberEmail });
-
-        reason = "";
-
-        if (group == null) {
-            reason = "Family Group not found";
-            throw err;
-        }
-
-        if (member == null) {
-            reason = "Member not found";
-            throw err;
-        }
-
-        //if new member, count should = 0 
-        const count = await FamilyGroups.find (
-            {
-                _id: groupId,
-                "groupMembers._id": member._id
-            }).count();
-
-        if (count >= 1) {
-            reason = "Member already in family.";
-            throw err;
-        }
-
-        await Users.updateOne({ _id: member._id },
-        {
-            $addToSet: {
-                groups: group,
-            },
-        });
-        
-        await FamilyGroups.updateOne({ _id: groupId },
-        {
-            $addToSet: {
-                groupMembers: member,
-            },
-        });
-
-        res.status(200).json({
-            status: "success",
-            message: "Added member to group",
-            data: {
-                group: group,
-            },
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: "fail",
-            message: reason + " Member not successfully added to group",
-        });
+    if (group == null) {
+      throw new Error((message = "Family Group not found"));
     }
+
+    if (member == null) {
+      throw new Error((message = "Member not found"));
+    }
+
+    //if new member, count should = 0 
+    const count = await FamilyGroups.countDocuments(
+      {
+          _id: groupId,
+          groupMembers: {
+              _id: member._id
+          }
+      });
+
+    if (count >= 1) {
+      throw new Error((message = "Member already in family."));
+    }
+
+    member = await Users.findByIdAndUpdate(member._id, {
+      $addToSet: {
+        groups: group,
+      },
+    }, { runValidators: true, new: true });
+
+    group = await FamilyGroups.findByIdAndUpdate(groupId, {
+      $addToSet: {
+        groupMembers: member,
+      },
+    }, { runValidators: true, new: true });
+
+    if (group !== null) {
+      res.status(200).send({
+        status: "success",
+        message: "Added member to group",
+        data: {
+          group: group,
+        },
+      });
+    };
+  } catch (err) {
+    res.status(404).send({
+      status: "fail",
+      message: err.message,
+      description: "Member not successfully added to group"
+    });
+  }
 };
 
 exports.getFamilyGroupEvents = async (req, res) => {
-    try {
-    
-        const { groupId } = req.body
-        const group = await FamilyGroups.findById({ _id: groupId }).populate('events');
-        reason = "";
-    
-        if (group == null) {
-          reason = "Family Group not found";
-          throw err;
-        }
-    
-        res.status(200).json({
-            status: "success",
-            message: "Found group",
-            data: {
-                groupEvents: group.events,
-            },
-        });
-      } catch (err) {
-    
-        res.status(404).json({
-            status: "fail",
-            message: "Group not found",
-        });
-      }
+  try {
+    const { groupId } = req.body;
+    const group = await FamilyGroups.findById(groupId).populate("events");
+
+    if (group == null) {
+      throw new Error((message = "Family Group not found"));
+    }
+
+    res.status(200).send({
+      status: "success",
+      message: "Found group",
+      data: {
+        groupEvents: group.events,
+      },
+    });
+  } catch (err) {
+    res.status(404).send({
+      status: "fail",
+      message: err.message,
+      description: "Group not found",
+    });
+  }
 };
 
 exports.leaveFamilyGroup = async (req, res) => {
+  try {
+    const { groupId, memberId } = req.body;
+    let group = await FamilyGroups.findById(groupId);
+    let member = await Users.findById(memberId);
+    successMessage = "";
+
+    //check if group and user are null
+    if (group == null) {
+      throw new Error((message = "Family Group not found"));
+    }
+
+    if (member == null) {
+      throw new Error((message = "User not found"));
+    }
+
+    //check if member array is empty
+    const initialMemberCount = group.groupMembers.length;
+    if (initialMemberCount <= 0) {
+      throw new Error((message = "Group does not have any members to remove"));
+    }
+
+    //remove member from the group's member array
     try {
-        const { groupId, memberId } = req.body
-        const group = await FamilyGroups.findOne({ _id: groupId });
-        const member = await Users.findOne({_id: memberId});
-        let updatedGroup = null;
-        reason = "";
-        successMessage = "";
-    
-        //check if group and user are null
-        if (group == null) {
-          reason = "Family Group not found";
-          throw err;
-        }
+      group = await FamilyGroups.findByIdAndUpdate(groupId, {
+        $pull: {
+          groupMembers: memberId,
+        },
+      });
+    } catch (err) {
+      throw new Error((message = "Member could not be removed from the group"));
+    }
 
-        if (member == null) {
-            reason = "User not found";
-            throw err;
-        }
+    //if member array is now empty, delete the group
+    try {
+      group = await FamilyGroups.findById(groupId);
 
-        //check if member array is empty
-        const initialMemberCount = group.groupMembers.length; 
-        if(initialMemberCount <= 0) {
-            reason = "Group does not have any members to remove";
-            throw err;
-        }
-
-        //remove member from the group's member array
-        try {
-            await FamilyGroups.updateOne({ _id: groupId },
-            {
-                $pull: {
-                    groupMembers: memberId,
-                },
-            });            
-        } catch (err) {
-            reason = "Member could not be removed from the group"
-            throw err;
-        }
-
-        //if member array is now empty, delete the group
-        try {
-            updatedGroup = await FamilyGroups.findOne({ _id: groupId });
-
-            if(updatedGroup.groupMembers.length == 0) {
-                await FamilyGroups.findOneAndRemove({ _id: groupId });
-
-                successMessage = " User was the last member of the group, so the group has been deleted"
-            } 
-        } catch (err) {
-            reason = "The last member of the group was removed, but the group could not be deleted"
-            throw err; 
-        }     
-
-        //remove the group from the user's groups array
-        try {
-            await Users.updateOne({ _id: memberId },
-            {
-                $pull: {
-                    groups: groupId,
-                },
-            });
-        } catch (err) {
-            reason = "Group could not be removed from the member"
-            throw err;
-        }
-    
-        res.status(200).json({
-            status: "success",
-            message: "User has successfully left the family group." + successMessage,
+      if (group.groupMembers.length == 0) {
+        group = await FamilyGroups.findByIdAndRemove(groupId, {
+          new: true
         });
-      } catch (err) {
-        res.status(404).json({
-            status: "fail",
-            message: reason,
-        });
+
+        successMessage =
+          " User was the last member of the group, so the group has been deleted";
       }
-};
+    } catch (err) {
+      throw new Error(
+        (message =
+          "The last member of the group was removed, but the group could not be deleted"),
+      );
+    }
 
+    //remove the group from the user's groups array
+    try {
+      await Users.findByIdAndUpdate(memberId, {
+        $pull: {
+          groups: groupId,
+        },
+      });
+    } catch (err) {
+      throw new Error((message = "Group could not be removed from the member"));
+    }
+
+    res.status(200).send({
+      status: "success",
+      message: "User has successfully left the family group." + successMessage,
+      data: group,
+    });
+
+  } catch (err) {
+    res.status(404).send({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
